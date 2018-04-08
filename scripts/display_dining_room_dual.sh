bb52b8d771eb33ae72fcd64f61cd6367ffce0092
Tyler Powers <gr4yscale@gmail.com>
2018-04-09 00:14:54 +0200

    display scripts

--- scripts/display_dining_room_dual.sh
+++ scripts/display_dining_room_dual.sh
@@ -10,13 +10,10 @@ MONITOR_DINING_ROOM="DVI-D-0"
 
 xrandr --output $MONITOR_LEFT --primary --mode 1920x1080 --rotate normal --dpi 96
 xrandr --output $MONITOR_RIGHT --mode 1920x1080 --same-as $MONITOR_LEFT --rotate normal --dpi 96
-xrandr --output $PROJECTOR_LIVING_ROOM --mode 1920x1080 --same-as $MONITOR_LEFT --rotate normal --dpi 96
+xrandr --output $PROJECTOR_LIVING_ROOM --mode 1920x1080 --right-of $MONITOR_LEFT --rotate normal --dpi 96
 xrandr --output $MONITOR_DINING_ROOM --mode 1920x1080 --same-as $MONITOR_LEFT --rotate normal --dpi 96
+xrandr --output $PROJECTOR_BEDROOM --mode 1280x720 --same-as $MONITOR_LEFT --rotate normal --dpi 96 \
 
-xrandr --output $PROJECTOR_BEDROOM --off
-xrandr --output $PROJECTOR_BEDROOM --auto --same-as $MONITOR_LEFT --rotate normal
-
-#xrandr --output $PROJECTOR_BEDROOM --mode 1280x720 --same-as $MONITOR_LEFT --rotate normal --dpi 96 \
 
 # all as one command:
 
